f8d1617e0b5c00a54c2faaed27e9783236cff299
tnkshuuhei <shuhei46491@gmail.com>
2022-10-07 17:03:49 +0900

    constructor causes a error

--- amm/lib.rs
+++ amm/lib.rs
@@ -9,26 +9,6 @@ mod amm {
     use ink_storage::collections::HashMap;
 
     // Part 1. Define Error enum 
-
-    // Part 2. Define storage struct 
-
-    // Part 3. Helper functions 
-
-    /// Defines the storage of your contract.
-    /// Add new fields to the below struct in order
-    /// to add new static storage fields to your contract.
-    #[derive(Default)]
-    #[ink(storage)]
-    pub struct Amm {
-        totalShares: Balance, // Stores the total amount of share issued for the pool
-        totalToken1: Balance, // Stores the amount of Token1 locked in the pool
-        totalToken2: Balance, // Stores the amount of Token2 locked in the pool
-        shares: HashMap<AccountId, Balance>, // Stores the share holding of each provider
-        token1Balance: HashMap<AccountId, Balance>, // Stores the token1 balance of each user
-        token2Balance: HashMap<AccountId, Balance>, // Stores the token2 balance of each user
-        fees: Balance,        // Percent of trading fees charged on trade
-    }
-
     #[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
     #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
     pub enum Error {
@@ -49,34 +29,49 @@ mod amm {
         /// Slippage tolerance exceeded
         SlippageExceeded,
     }
-    #[ink(impl)]
-    impl Amm {
-    // Ensures that the _qty is non-zero and the user has enough balance
-    fn validAmountCheck(
-        &self,
-        _balance: &HashMap<AccountId, Balance>,
-        _qty: Balance,
-    ) -> Result<(), Error> {
-        let caller = self.env().caller();
-        let my_balance = *_balance.get(&caller).unwrap_or(&0);
-
-        match _qty {
-            0 => Err(Error::ZeroAmount),
-            _ if _qty > my_balance => Err(Error::InsufficientAmount),
-            _ => Ok(()),
+        // Part 2. Define storage struct 
+        #[derive(Default)]
+        #[ink(storage)]
+        pub struct Amm {
+            totalShares: Balance, // Stores the total amount of share issued for the pool
+            totalToken1: Balance, // Stores the amount of Token1 locked in the pool
+            totalToken2: Balance, // Stores the amount of Token2 locked in the pool
+            shares: HashMap<AccountId, Balance>, // Stores the share holding of each provider
+            token1Balance: HashMap<AccountId, Balance>, // Stores the token1 balance of each user
+            token2Balance: HashMap<AccountId, Balance>, // Stores the token2 balance of each user
+            fees: Balance,        // Percent of trading fees charged on trade
         }
-    }
 
-    // Returns the liquidity constant of the pool
-    fn getK(&self) -> Balance {
-        self.totalToken1 * self.totalToken2
-    }
+        
+        // Part 3. Helper functions 
+        #[ink(impl)]
+        impl Amm {
+        // Ensures that the _qty is non-zero and the user has enough balance
+        fn validAmountCheck(
+            &self,
+            _balance: &HashMap<AccountId, Balance>,
+            _qty: Balance,
+        ) -> Result<(), Error> {
+            let caller = self.env().caller();
+            let my_balance = *_balance.get(&caller).unwrap_or(&0);
 
-    // Used to restrict withdraw & swap feature till liquidity is added to the pool
-    fn activePool(&self) -> Result<(), Error> {
-        match self.getK() {
-            0 => Err(Error::ZeroLiquidity),
-            _ => Ok(()),
+            match _qty {
+                0 => Err(Error::ZeroAmount),
+                _ if _qty > my_balance => Err(Error::InsufficientAmount),
+                _ => Ok(()),
+            }
+        }
+        // Returns the liquidity constant of the pool
+        fn getK(&self) -> Balance {
+            self.totalToken1 * self.totalToken2
+        }
+
+        // Used to restrict withdraw & swap feature till liquidity is added to the pool
+        fn activePool(&self) -> Result<(), Error> {
+            match self.getK() {
+                0 => Err(Error::ZeroLiquidity),
+                _ => Ok(()),
+            }
         }
     }
         // Part 4. Constructor
@@ -89,7 +84,7 @@ mod amm {
                 fees: if _fees >= 1000 { 0 } else { _fees },
                 ..Default::default()
             }
-        }        
+        }
         // Part 5. Faucet
         /// Sends free token(s) to the invoker
         #[ink(message)]
@@ -318,34 +313,7 @@ mod amm {
                 .and_modify(|val| *val += _amountToken2);
             Ok(amountToken1)
         }
-        /// Constructor that initializes the `bool` value to the given `init_value`.
-        #[ink(constructor)]
-        pub fn new(init_value: bool) -> Self {
-            Self { value: init_value }
-        }
-
-        /// Constructor that initializes the `bool` value to `false`.
-        ///
-        /// Constructors can delegate to other constructors.
-        #[ink(constructor)]
-        pub fn default() -> Self {
-            Self::new(Default::default())
-        }
-
-        /// A message that can be called on instantiated contracts.
-        /// This one flips the value of the stored `bool` from `true`
-        /// to `false` and vice versa.
-        #[ink(message)]
-        pub fn flip(&mut self) {
-            self.value = !self.value;
-        }
-
-        /// Simply returns the current value of our `bool`.
-        #[ink(message)]
-        pub fn get(&self) -> bool {
-            self.value
-        }
-    }
+    
 
     /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
     /// module and test functions are marked with a `#[test]` attribute.
